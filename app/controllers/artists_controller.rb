2af0743b24369830a436306213bc81356bf6b26f
Luiz Mo <luizmorey@gmail.com>
2019-11-14 15:28:12 -0700

    first commit

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,48 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  end
+
+  def new 
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path #sends you back to index
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update 
+    if @artist.update(artist_params)
+      redirect_to artist_path(@artist) #sends you back to show
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artist_path
+  end
+
+  private
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+      params.require(:artist).permit(:name, :avatar)
+    end
+end
